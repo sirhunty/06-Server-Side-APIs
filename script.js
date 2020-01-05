309aebd0da39acf342016b4b0d20361720ac49ed
Christopher Matthews <sirhunty@gmail.com>
2020-01-05 16:30:32 -0700

    updated js file displaying data of storage or api call on page, and reformating openweather date

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ searchHistory();
 
 // Initial city user search of Today's and the weekly weather 
 // data
-$("#citySearchBtn").on("click", function(event) {
+$("#citySearchBtn").on("click", function (event) {
   event.preventDefault();
   let citySearched = $("#citySearchText").val();
   $("#citySearchText").val("");
@@ -14,21 +14,21 @@ $("#citySearchBtn").on("click", function(event) {
 // Taking the previous user city choice and putting the in the
 // historical list
 function searchHistory() {
-    $("#searchHistoryList").empty();
-    let searchHistory = getStoredWeatherData().searchHistory;
-    if (searchHistory) {
-      for (let i = 0; i < searchHistory.length; i++) {
-        let item = $("<li class='list-group-item'></li>");
-        item.text(searchHistory[i].cityName);
-        $("#searchHistoryList").prepend(item);
-      }
-      $(".list-group-item").on("click", function() {
-        getCurrentWeatherConditions($(this).text());
-        getWeeklyForecast($(this).text());
-        console.log(searchHistory); //testing searchHistory
-      });
+  $("#searchHistoryList").empty();
+  let searchHistory = getStoredWeatherData().searchHistory;
+  if (searchHistory) {
+    for (let i = 0; i < searchHistory.length; i++) {
+      let item = $("<li class='list-group-item'></li>");
+      item.text(searchHistory[i].cityName);
+      $("#searchHistoryList").prepend(item);
     }
+    $(".list-group-item").on("click", function () {
+      getCurrentWeatherConditions($(this).text());
+      getWeeklyForecast($(this).text());
+      console.log(searchHistory); //testing searchHistory
+    });
   }
+}
 // Populates the user's last searches or returns empy if no data
 function getStoredWeatherData() {
   let storedWeatherData = JSON.parse(localStorage.getItem("storedWeatherData"));
@@ -76,7 +76,7 @@ function getCurrentWeatherConditions(citySearched) {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(results) {
+  }).then(function (results) {
     populateCurrentWeatherConditions(results);
     storeCurrentWeather(results);
     console.log(results);
@@ -107,7 +107,7 @@ function populateCurrentWeatherConditions(results) {
   let lon = results.coord.lon;
   let lat = results.coord.lat;
 
-// Adds data to the html/page
+  // Adds data to the html/page
   $("#currentCity").text(cityName);
   $("#todaysDate").text(
     `(${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()})`
@@ -127,7 +127,7 @@ function populateUVIndex(lon, lat) {
   $.ajax({
     url: UVIndexURL,
     method: "GET"
-  }).then(function(results) {
+  }).then(function (results) {
     let UVIndex = results.value;
     let currUVLevel = $("#todaysUVIndex").attr("data-uv-level");
     $("#todaysUVIndex").removeClass(currUVLevel);
@@ -159,7 +159,7 @@ function getWeeklyForecast(citySearched) {
     ).getDate();
     if (
       storedWeatherData.searchHistory[i].cityName.toLowerCase() ==
-        citySearched &&
+      citySearched &&
       savedDate == today
     ) {
       for (let j = 0; j < storedWeatherData.data.forecast.length; j++) {
@@ -177,7 +177,7 @@ function getWeeklyForecast(citySearched) {
   $.ajax({
     url: forecastURL,
     method: "GET"
-  }).then(function(results) {
+  }).then(function (results) {
     populateForecast(results);
     storeForecast(results, citySearched);
   });
@@ -191,5 +191,50 @@ function storeForecast(results, citySearched) {
   storedWeatherData.data.forecast.push(results);
   localStorage.setItem("storedWeatherData", JSON.stringify(storedWeatherData));
 }
+
+
+// Takes storage or API data and displays on page
+function populateForecast(results) {
+  $("#forecast").empty();
+  let list = results.list;
+  let daysForecasted = 0;  // Checks for only 5 days showing
+  for (let i = 0; i < list.length && daysForecasted < 6; i++) {
+    let time = list[i].dt_txt.split(" ")[1];
+
+    if (time == "12:00:00") {
+      let cardDiv = $("<div class='card forecast-card mx-2 shadow'>");
+      let cardBodyDiv = $("<div class='card-body'>");
+
+      let dateDiv = $("<div class='forecast-date'>");
+      let date = formatDate(list[i].dt_txt.split(" ")[0]);
+      dateDiv.text(date);
+
+      let imgElem = $("<img>");
+      let iconURL = `https://openweathermap.org/img/w/${list[i].weather[0].icon}.png`;
+      imgElem.attr("src", iconURL);
+      let temp = list[i].main.temp;
+      let pTemp = $(`<p>Temp: ${temp} &degF</p>`);
+      let humidity = list[i].main.humidity;
+      let pHumid = $(`<p>Humidity: ${humidity}%</p>`);
+
+      cardBodyDiv.append(dateDiv, imgElem, pTemp, pHumid);
+      cardDiv.append(cardBodyDiv);
+      $("#forecast").append(cardDiv);
+
+      daysForecasted++;
+    }
+  }
+  populateSearchHistory();
+}
+
+// Formating openweathermaps API date format to "MM/DD/YYY"
+function formatDate(date) {
+  let arr = date.split(" ")[0].split("-");
+  let formattedDate = `${arr[1]}/${arr[2]}/${arr[0]}`;
+  return formattedDate;
+}
+
+// Clears locale storage
+localStorage.clear();
 
 
