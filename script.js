8d3ff3d60a5a971cba351e79740333663fa101be
Christopher Matthews <sirhunty@gmail.com>
2020-01-05 13:16:58 -0700

    added begginning of the script.js file

---
+++ script.js
@@ -0,0 +1,32 @@
+const APIKEY = "ce0409395dee9ab9aa9cab9f0bb10142";
+searchHistory();
+
+// Initial city user search of Today's and the weekly weather 
+// data
+$("#citySearchBtn").on("click", function(event) {
+  event.preventDefault();
+  let citySearched = $("#citySearchText").val();
+  $("#citySearchText").val("");
+  getCurrentWeatherConditions(citySearched);
+  getWeeklyForecast(citySearched);
+});
+
+// Taking the previous user city choice and putting the in the
+// historical list
+function searchHistory() {
+    $("#searchHistoryList").empty();
+    let searchHistory = getStoredWeatherData().searchHistory;
+    if (searchHistory) {
+      for (let i = 0; i < searchHistory.length; i++) {
+        let item = $("<li class='list-group-item'></li>");
+        item.text(searchHistory[i].cityName);
+        $("#searchHistoryList").prepend(item);
+      }
+      $(".list-group-item").on("click", function() {
+        getCurrentWeatherConditions($(this).text());
+        getWeeklyForecast($(this).text());
+        console.log(searchHistory); //testing searchHistory
+      });
+    }
+  }
+
